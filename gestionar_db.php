e77dd0425d9aafb48aaa441395398d029060810e
brahian.verac <brahian.verac@gmail.com>
2017-10-10 12:20:17 -0500

    corrergi error al conectarse a la base de datos

--- gestionar_db.php
+++ gestionar_db.php
@@ -5,13 +5,12 @@ class Gestionar_db{
     function __construct($host_name,$user_name,$password,$database_name){
         $this->con = mysqli_connect($host_name,$user_name,$password,$database_name);
         if(!$this->con){
-            die("Conexion fallida".mysqli_connect_erro());
+            die("Conexion fallida".mysqli_connect_error());
         }
     }
     function insertar_datos($email,$name_user,$apellido,$codigo){
         $sql = "INSERT INTO usuarios VALUES (NULL, '".$email."', '".$name_user."', '".$apellido."', '".$codigo."')";
         if(mysqli_query($this->con,$sql)){
-            echo "ha sido registrado un nuevo usuario";
         }else{
             echo "Error al ingresar un nuevo usuario";
         }
