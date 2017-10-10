a3a784ac383638790707b57a637f77f37cc207e1
brahian.verac <brahian.verac@gmail.com>
2017-10-10 09:57:34 -0500

    datos a la conexion de la base de datos

---
+++ config.php
@@ -0,0 +1,7 @@
+<!-- vim: sw=4 ts=4 expandtab -->
+<?php
+    //En este archivo van los datos a la conexion de la base de datos
+    $G_HOST_NAME = "localhost";
+    $G_USER_NAME = "root";
+    $G_PASSWORD = "NACIONAL10.";
+    $G_DATABASE_NAME = "jerrejerre";
